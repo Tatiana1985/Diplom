6b27593f1c821586b3136f9e3674ada84322f8ae
Tatiana1985 <tanya198511@inbox.ru>
2022-09-30 01:45:17 +0300

    Совместимость node

--- index.js
+++ index.js
@@ -1,53 +1,54 @@
-const express = require('express')
+import express from "express";
+import { dirname } from "path";
+import { fileURLToPath } from "url";
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
 
 const app = express();
-app.use('/static', express.static(__dirname));
+app.use("/static", express.static(__dirname));
 // создаем парсер для данных в формате json
 const jsonParser = express.json();
 
 const users = [
-    {
-        email: 'AAA@rr',
-        password: 'AAA',
-    },
-    {
-        email: 'BBB@B',
-        password: 'BBB',
-    },
-    {
-        email: 'CCC@rr',
-        password: 'CCC',
-    }
-]
+	{
+		email: "AAA@rr",
+		password: "AAA",
+	},
+	{
+		email: "BBB@B",
+		password: "BBB",
+	},
+	{
+		email: "CCC@rr",
+		password: "CCC",
+	},
+];
 // Сюда приходят логин и пароль пользователя на проверку
 app.post("/user", jsonParser, function (request, response) {
-    let data = request.body;
-    if(!request.body) return response.sendStatus(400);
-    let answer = {
-        validUser: false
-    }
-    users.forEach(user => {
-        if (user.email === data.email && user.password === data.pass) {
-            answer.validUser = true;
-        }
-    })
-    response.json(answer); // отправляем пришедший ответ обратно
+	let data = request.body;
+	if (!request.body) return response.sendStatus(400);
+	let answer = {
+		validUser: false,
+	};
+	users.forEach((user) => {
+		if (user.email === data.email && user.password === data.pass) {
+			answer.validUser = true;
+		}
+	});
+	response.json(answer); // отправляем пришедший ответ обратно
 });
 
-
-app.get("/cakes", function(request, response){
-    console.log('редирект прошел')
-    response.sendFile(__dirname + "/views/cakes.html");
+app.get("/cakes", function (request, response) {
+	console.log("редирект прошел");
+	response.sendFile(__dirname + "/views/cakes.html");
 });
 
-
-app.get("/", function(request, response){
-
-    response.sendFile(__dirname + "/views/index.html");
+app.get("/", function (request, response) {
+	response.sendFile(__dirname + "/views/index.html");
 });
 
-app.get("/reg", function(request, response){
-    response.sendFile(__dirname + "/views/reg.html");
+app.get("/reg", function (request, response) {
+	response.sendFile(__dirname + "/views/reg.html");
 });
 
 app.listen(3000);
